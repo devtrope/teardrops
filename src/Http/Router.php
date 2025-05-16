27e8c04b612437019d1d42be50ff3878744fabd5
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-05-16 14:30:55 +0200

    feat: Check parameters for specified method

--- src/Http/Router.php
+++ src/Http/Router.php
@@ -25,6 +25,33 @@ class Router
             throw new \Exception("Method not callable: $methodName in $controllerClass");
         }
 
-        call_user_func_array($callable, $route->parameters());
+        $parameters = self::resolveParameters($controllerInstance, $methodName, $route->parameters());
+        call_user_func_array($callable, $parameters);
+    }
+
+    public static function resolveParameters(object $controller, string $methodName, array $parameters): array
+    {
+        $reflection = new \ReflectionMethod($controller, $methodName);
+        $expectedParameters = $reflection->getParameters();
+
+        if (count($parameters) > count($expectedParameters)) {
+            throw new \Exception("Too many parameters provided for method: $methodName");
+        }
+
+        $resolvedParameters = [];
+
+        foreach ($expectedParameters as $index => $parameter) {
+            if (array_key_exists($index, $parameters)) {
+                $resolvedParameters[] = $parameters[$index];
+            } elseif ($parameter->isDefaultValueAvailable()) {
+                $resolvedParameters[] = $parameter->getDefaultValue();
+            } elseif ($parameter->allowsNull()) {
+                $resolvedParameters[] = null;
+            } else {
+                throw new \Exception("Missing required parameter: " . $parameter->getName());
+            }
+        }
+
+        return $resolvedParameters;
     }
 }
--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,11 @@
 
 namespace Teardrops\Teardrops;
 
+use Teardrops\Teardrops\Support\Config;
+use Teardrops\Teardrops\Support\Events;
 use Teardrops\Teardrops\Http\Request;
 use Teardrops\Teardrops\Http\Router;
 use Teardrops\Teardrops\Http\Route;
-use Teardrops\Teardrops\Support\Events;
 
 class Kernel
 {
@@ -20,6 +21,14 @@ class Kernel
             );
         } catch (\Exception $e) {
             http_response_code(404);
+
+            if (Config::get('DEBUG', false)) {
+                print_r('<pre>');
+                print_r($e->getMessage());
+                print_r($e->getTraceAsString());
+                print_r('</pre>');
+            }
+
             Events::dispatch('page_not_found');
             exit;
         }
