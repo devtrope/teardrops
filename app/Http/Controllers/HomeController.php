d68d651758372cb5c0a8e9aa382dc691d14f4f09
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-05-23 16:36:52 +0200

    Default page when initialized

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Composer\InstalledVersions;
+
 class HomeController
 {
     public function getIndex()
     {
         return view('base.html.twig', [
             'title' => 'Home',
-            'version' => '0.0.2',
+            'version' => InstalledVersions::getPrettyVersion('teardrops/framework'),
             'features' => [
                 ['title' => 'Routing', 'desc' => 'Map HTTP methods to controller methods automatically.'],
                 ['title' => 'Views', 'desc' => 'Twig-based rendering with clean templating logic.'],
