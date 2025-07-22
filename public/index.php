737dcf9aa66a962506810ff58a4d9ee86339d476
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-07-22 15:43:16 +0200

    feat: add environment files

--- public/index.php
+++ public/index.php
@@ -2,5 +2,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$envFile = \Teardrops\Teardrops\Config\Config::isDevelopment() ? '.env.local' : '.env';
+$dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__), $envFile);
+$dotenv->load();
+
 \Teardrops\Teardrops\Http\Model::initialize();
 \Teardrops\Teardrops\Config\Routing\Router::run($_SERVER['REQUEST_URI']);
--- src/Config/Database.php
+++ src/Config/Database.php
@@ -13,9 +13,12 @@ class Database
         try {
             if (self::$instance === null) {
                 return self::$instance = new PDO(
-                    'mysql:host=localhost;dbname=trackbox',
-                    'root',
-                    'root',
+                    "mysql:host={$_ENV['DATABASE_HOST']};
+                    dbname={$_ENV['DATABASE_DB']};
+                    charset=utf8mb4;
+                    port={$_ENV['DATABASE_PORT']}",
+                    $_ENV['DATABASE_USER'],
+                    $_ENV['DATABASE_PWD'],
                     [
                         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
