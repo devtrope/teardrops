76d6989b0699c62a4271ffa76f4d582fd8fcf1d3
Quentin SCHIFFERLE <q.schifferle@tod.fr>
2025-10-23 13:32:20 +0200

    feat: update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/helpers.php';
 
 use Teardrops\Teardrops\Request;
 use Teardrops\Teardrops\Route;
