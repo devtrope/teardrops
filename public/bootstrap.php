2afde3ebfe2f7ba1f8fa5f03f88c53f688fbddb6
Quentin SCHIFFERLE <q.schifferle@tod.fr>
2025-10-24 11:53:32 +0200

    feat: cache system for the routes in production

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -1,10 +1,13 @@
 <?php
 
 define ('TEMPLATES_PATH', dirname(__DIR__) . '/templates');
-define ('ROUTES_PATH', dirname(__DIR__) . '/routes/routes.php');
+define ('ROUTES_PATH', dirname(__DIR__) . '/web/routes.php');
+define ('CACHE_PATH', dirname(__DIR__) . '/web/cache');
 
-if (! file_exists(ROUTES_PATH)) {
-    throw new \Exception('Routes file not found at ' . ROUTES_PATH);
+if ($_ENV['APP_ENVIRONMENT'] !== 'production') {
+    if (! file_exists(ROUTES_PATH)) {
+        throw new \Exception('Routes file not found at ' . ROUTES_PATH);
+    }
+    
+    require ROUTES_PATH;
 }
-
-require ROUTES_PATH;
--- public/index.php
+++ public/index.php
@@ -5,6 +5,16 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$environmentFile = __DIR__ . '/../.env';
+
+if (! file_exists($environmentFile)) {
+    throw new Exception('.env file is missing. Please create it based on the .env.example file.');
+}
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 require __DIR__ . '/../src/helpers.php';
 require __DIR__ . '/bootstrap.php';
 
