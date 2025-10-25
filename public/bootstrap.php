66d6759e2ebfe98d8f8955977d6da2677a092abf
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-10-25 09:11:31 +0200

    refactor: move multiple loadings into bootstrap file

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -4,6 +4,15 @@ define ('TEMPLATES_PATH', dirname(__DIR__) . '/templates');
 define ('ROUTES_PATH', dirname(__DIR__) . '/web/routes.php');
 define ('CACHE_PATH', dirname(__DIR__) . '/web/cache');
 
+$environmentFile = __DIR__ . '/../.env';
+
+if (! file_exists($environmentFile)) {
+    throw new Exception('.env file is missing. Please create it based on the .env.example file.');
+}
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 if ($_ENV['APP_ENVIRONMENT'] !== 'production') {
     if (! file_exists(ROUTES_PATH)) {
         throw new \Exception('Routes file not found at ' . ROUTES_PATH);
@@ -11,3 +20,7 @@ if ($_ENV['APP_ENVIRONMENT'] !== 'production') {
     
     require ROUTES_PATH;
 }
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
--- public/index.php
+++ public/index.php
@@ -5,21 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 require __DIR__ . '/../vendor/autoload.php';
-
-$environmentFile = __DIR__ . '/../.env';
-
-if (! file_exists($environmentFile)) {
-    throw new Exception('.env file is missing. Please create it based on the .env.example file.');
-}
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
 require __DIR__ . '/../src/helpers.php';
 require __DIR__ . '/bootstrap.php';
-
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
 
 \Ludens\Core\Application::init(new Ludens\Http\Request());
