ab3370efa39f973e0a0ec567c6bc5b6a8f35f2c0
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-11-11 10:09:40 +0100

    test: add item to cart

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::call([\App\Http\Controller\ShopController::class, 'index'])->when('/shop'
 Route::call([\App\Http\Controller\ProductController::class, 'index'])->when('/products')->onPost();
 Route::call([\App\Http\Controller\ProductController::class, 'display'])->when('/products/{id}')->onGet();
 Route::call([\App\Http\Controller\CartController::class, 'index'])->when('/cart')->onGet();
+Route::call([\App\Http\Controller\CartController::class, 'add'])->when('/cart/add')->onPost();
--- src/Http/Controller/CartController.php
+++ src/Http/Controller/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controller;
 use App\Http\Model\Cart;
 use Ludens\Database\ModelManager;
 use Ludens\Framework\Controller\AbstractController;
+use Ludens\Http\Request;
 use Ludens\Http\Response;
 
 class CartController extends AbstractController
@@ -13,5 +14,26 @@ class CartController extends AbstractController
     {
         $cart = $modelManager->get(Cart::class)->all();
         return $this->render('cart/index', ['cart' => $cart]);
+    }
+
+    public function add(ModelManager $modelManager, Request $request): Response
+    {
+        $currentCart = $modelManager->get(Cart::class)::query()->where('product_id', $request->product)->first();
+
+        if ($currentCart) {
+            $cart = $modelManager->get(Cart::class)->find($currentCart['id']);
+            $cart->product_id = $request->product;
+            $cart->quantity = $cart->quantity + 1;
+            $cart->update();
+
+            return $this->json(['success' => true, 'message' => 'Product added to cart.']);
+        }
+
+        $cart = new Cart();
+        $cart->product_id = $request->product;
+        $cart->quantity = 1;
+        $cart->save();
+
+        return $this->json(['success' => true, 'message' => 'Product added to cart.']);
     }
 }
