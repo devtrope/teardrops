ad4e966a23c9506840901b203f35a8fb6e2b7f35
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-11-13 17:01:52 +0100

    feat: add global BaseController for the Controllers

---
+++ src/Http/Controller/BaseController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controller;
+
+use Ludens\Framework\Controller\AbstractController;
+
+class BaseController extends AbstractController
+{
+    // Add your global methods here
+}
